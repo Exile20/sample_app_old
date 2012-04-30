481b935157d043115254d0008953a41fa515b1eb
Chadwick Springer <exile20@hotmail.com>
2012-04-29 22:53:56 -0400

    ch11ex

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -57,6 +57,19 @@ describe "GET 'index'" do
         response.should have_selector("a", :href => "/users?page=2",
                                            :content => "Next")
       end
+
+      it "should paginate the microposts" do 
+        32.times do 
+        mp = Factory(:micropost, :user => @user, :content => "fooooo baaaar") 
+      end 
+       get :show, :id => @user 
+       response.should have_selector("div.pagination") 
+       response.should have_selector("span.disabled", :content => "Previous") 
+       response.should have_selector("a", :href => "/users/1?page=2", 
+       :content => "2") 
+       response.should have_selector("a", :href => "/users/1?page=2", 
+       :content => "Next") 
+      end
     end
   end
 
